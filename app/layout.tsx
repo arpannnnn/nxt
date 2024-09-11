2f08e404df34e795a479e752b3288eb1f10b3056
Arpan <rockarpan31@gmail.com>
2024-09-11 19:48:22 +0545

    convex setup done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import Navbar from "../components/navbar";
 
 const geistSans = localFont({
@@ -26,6 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ConvexAuthNextjsServerProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -39,5 +41,6 @@ export default function RootLayout({
 
       </body>
     </html>
+    </ConvexAuthNextjsServerProvider>
   );
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+"use client";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
+export default function Home() {
+    const tasks = useQuery(api.tasks.get);
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+        </main>
+    );
 }
-
-export default page
